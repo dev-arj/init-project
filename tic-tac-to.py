23f2e3cad771666a9c48eab3eaf91b8050b02234
arj_s <rockztar003@gmail.com>
2021-04-29 23:14:36 +0530

    Tic Tac Toe dev is in progress

--- tic-tac-to.py
+++ tic-tac-to.py
@@ -1,5 +1,25 @@
 width = 13
 height = 3
+def PlayerProfile():
+    player1 = str(input("Enter the name of Player One: "))
+    player2 = str(input("Enter the name of Player Two: "))
+    return [player1, player2]
+print("The game begins here!")
+players = PlayerProfile()
+print(f'{players[0]} plays "X" and {players[1]} plays with "O"')
+Soman = True
+XPlay = True
+positionList = [0,0]
+while Soman:
+    if XPlay:    
+        positionList[0] = int(input())
+        positionList[1] = int(input())
+    continew = str(input("Enter Q to exit!"))
+    if continew == 'q' or continew == 'Q':
+        Soman = False
+        print("The End!!!")
+
+
 print('-' * width)
 for i in range(height):
     print('|',  f'{i:^3d}' * 3, '|')
